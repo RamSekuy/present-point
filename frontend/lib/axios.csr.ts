59a15f1d23900dff5dc546210e1db5866bb8fcb4
RamSekuy <ramaalim34@gmail.com>
2026-02-09 19:01:44 +0700

    feat: add config

--- frontend/lib/axios.csr.ts
+++ frontend/lib/axios.csr.ts
@@ -1,10 +1,11 @@
+import { BASE_API_URL } from "@/config/config";
 import axios, { AxiosInstance } from "axios";
 import { getCookie } from "cookies-next";
 
 export function axiosCSR(): AxiosInstance {
   const token = getCookie("access_token");
   return axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
+    baseURL: BASE_API_URL,
     withCredentials: true,
     headers: {
       Authorization: `Bearer ${token}`,
--- frontend/lib/axios.ssr.ts
+++ frontend/lib/axios.ssr.ts
@@ -1,10 +1,11 @@
+import { BASE_API_URL } from "@/config/config";
 import axios from "axios";
 import { cookies } from "next/headers";
 
 export async function axiosSSR() {
-  const token = (await cookies()).get("access_token")?.value || "";
+  const token = (await cookies()).get("access_token")?.value || "none";
   return axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
+    baseURL: BASE_API_URL,
     withCredentials: true,
     headers: {
       Authorization: `Bearer ${token}`,
